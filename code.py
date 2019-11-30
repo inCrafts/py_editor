82299919890423962e102f3952bf4d21c38d1f3c
inCrafts <inCrafts@mail.ru>
2019-11-30 20:18:45 +0300

    Добавлено 'О прграмме' +сохранение

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
 
 root = Tk()
-root.title('simNotepad')
+root.title('simNote')
+root.iconbitmap('img/simNote.ico')
 root.geometry('800x600+120+200')
 
 textframe = Frame(root, bg='#3C3F41')
@@ -17,7 +20,7 @@ def del_str():
 
 
 def about():
-    print('О программе')
+    messagebox.showinfo('About simNote', 'Программа simNote, версия 1.0')
 
 
 def theme_changer(theme):
@@ -27,17 +30,45 @@ def theme_changer(theme):
     textarea['selectbackground'] = themes[theme]['select_bg']
 
 
+def root_quit():
+    answer = messagebox.askokcancel(title='Выход', message='Закрыть программу?')
+    if answer:
+        root.destroy()
+
+
+def file_open():
+    file_path = filedialog.askopenfilename(title='Открыть файл', filetypes=(('Текстовые файлы(*.txt)', '*.txt'), ('Все файлы(*.*)', '*.*')))
+    if file_path:
+        textarea.delete('1.0', END)
+        textarea.insert('1.0', open(file_path, encoding='utf-8').read())
+
+
+def file_save():
+    file_path = filedialog.asksaveasfilename(title='Сохранить файл', filetypes=(('Текстовые файлы(*.txt)', '*.txt'), ('Все файлы(*.*)', '*.*')))
+    newfile = open(file_path, 'w', encoding='utf-8')
+    text = textarea.get('1.0', END)
+    newfile.write(text)
+    newfile.close()
+
+
+def file_save_as():
+    file_path = filedialog.asksaveasfilename(title='Сохранить файл как', filetypes=(('Текстовые файлы(*.txt)', '*.txt'), ('Все файлы(*.*)', '*.*')))
+    newfile = open(file_path, 'w', encoding='utf-8')
+    text = textarea.get('1.0', END)
+    newfile.write(text)
+    newfile.close()
+
+
 # File nav
 
 file_nav = Menu(top_nav, tearoff=0)
 file_nav.add_command(label='Создать')
-file_nav.add_command(label='Открыть')
-file_nav.add_command(label='Сохранить')
-file_nav.add_command(label='Сохранить как')
+file_nav.add_command(label='Открыть', command=file_open)
+file_nav.add_command(label='Сохранить', command=file_save)
+file_nav.add_command(label='Сохранить как', command=file_save_as)
 file_nav.add_separator()
-file_nav.add_command(label='Выход')
+file_nav.add_command(label='Выход', command=root_quit)
 top_nav.add_cascade(label='Файл', menu=file_nav)
-
 
 # Edit nav
 
@@ -54,18 +85,6 @@ edit_subnav.add_command(label='Вырезать')
 edit_subnav.add_command(label='Вставить')
 edit_nav.add_cascade(label='Копипаст', menu=edit_subnav)
 top_nav.add_cascade(label='Редактировать', menu=edit_nav)
-
-
-# Help nav
-
-help_nav = Menu(top_nav, tearoff=0)
-theme_subnav = Menu(help_nav, tearoff=0)
-theme_subnav.add_command(label='Онлайн')
-theme_subnav.add_command(label='Оффлайн')
-help_nav.add_cascade(label='Помощь', menu=theme_subnav)
-help_nav.add_separator()
-help_nav.add_command(label='О прграмме', command=about)
-top_nav.add_cascade(label='Помощь', menu=help_nav)
 
 # Themes
 
@@ -84,7 +103,6 @@ themes = {
         'select_bg': '#777777',
     }
 }
-
 
 # Themes nav
 
@@ -106,6 +124,18 @@ dev_subnav.add_command(label='Cobalt')
 themes_nav.add_cascade(label='В разработке', menu=dev_subnav)
 top_nav.add_cascade(label='Темы', menu=themes_nav)
 
+# Help nav
+
+help_nav = Menu(top_nav, tearoff=0)
+theme_subnav = Menu(help_nav, tearoff=0)
+theme_subnav.add_command(label='Онлайн')
+theme_subnav.add_command(label='Оффлайн')
+help_nav.add_cascade(label='Помощь', menu=theme_subnav)
+help_nav.add_separator()
+help_nav.add_command(label='О прграмме', command=about)
+top_nav.add_cascade(label='Помощь', menu=help_nav)
+
+
 
 textarea = Text(textframe, bg=themes['dark']['text_bg'], fg=themes['dark']['text_fg'], width=30, padx=10, pady=10, wrap=WORD, insertbackground=themes['dark']['cursor'], selectbackground=themes['dark']['select_bg'], spacing3=7)
 # textarea = Text(textframe, bg='#343d46', fg='#cccccc', width=30, padx=10, pady=10, wrap=WORD, insertbackground='#eda756', selectbackground='#4e5a65', spacing3=5)
@@ -116,27 +146,3 @@ scroll.pack(fill=Y, side=LEFT)
 textarea.config(yscrollcommand=scroll.set)
 
 root.mainloop()
-
-
-# def add_str():
-#     textarea.insert('2.4', 'Hello')
-#
-#
-# def del_str():
-#     textarea.delete('1.0', END)
-#
-#
-# def get_str():
-#     print(textarea.get('1.0', END))
-#
-#
-# btn_add  = Button(root, text='Add', command=add_str).place(x=50, y=10)
-# btn_del  = Button(root, text='Delete', command=del_str).place(x=90, y=10)
-# btn_get  = Button(root, text='Get', command=get_str).place(x=140, y=10)
-#
-# textarea = Text(f_text, bg='#343d46', fg='#cccccc', width=30, padx=10, pady=10, wrap=WORD, insertbackground='#eda756', selectbackground='#4e5a65', spacing3=5)
-# textarea.pack(fill=BOTH, expand=1, side=LEFT)
-#
-# scroll = Scrollbar(f_text, command=textarea.yview)
-# scroll.pack(fill=Y, side=LEFT)
-# textarea.config(yscrollcommand=scroll.set)
